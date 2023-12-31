a2320a0b5396f9ff932dbaf46bc6ad6541556c02
kaungKhantToe <146678182+kaungKhantToe@users.noreply.github.com>
2023-12-31 18:41:10 -0500

    schema.sql
    naming convention for first and last names

--- Database/schema.sql
+++ Database/schema.sql
@@ -9,8 +9,8 @@ CREATE TABLE courses (
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(100) NOT NULL,
-    lastname VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
     desired_section_name VARCHAR(100) NOT NULL,
